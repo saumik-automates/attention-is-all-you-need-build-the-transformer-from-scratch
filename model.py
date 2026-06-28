2884b40ee35b6c00952690852b434a82f57a30b1
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-06-28 12:29:52 +0530

    Step 14: build_padding_mask

--- model.py
+++ model.py
@@ -127,8 +127,14 @@ def add_positional_encoding_to_embeddings(embedded_batch, positional_encoding):
     L = embedded_batch.shape[1]
     return embedded_batch + positional_encoding[:L, :]
 
-# Step 14 - build_padding_mask (not yet solved)
-# TODO: implement
+# Step 14 - build_padding_mask
+import torch
+
+def build_padding_mask(token_ids, pad_id):
+    """Return a (B, 1, 1, L) bool mask: True where token_ids != pad_id."""
+    attn_mask = (token_ids != pad_id)
+    B, L = token_ids.shape[0], token_ids.shape[1]
+    return attn_mask.reshape((B, 1, 1, L))
 
 # Step 15 - build_causal_mask (not yet solved)
 # TODO: implement
