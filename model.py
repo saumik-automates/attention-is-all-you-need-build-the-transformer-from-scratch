a1cc51446c871ebd7c4340c0198df49a60a331fd
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-06-30 20:08:59 +0530

    Step 33: apply_ffn_second_linear

--- model.py
+++ model.py
@@ -262,8 +262,11 @@ def assemble_multi_head_attention_forward(query, key, value, w_q, w_k, w_v, w_o,
 def apply_ffn_first_linear_and_relu(x, w1, b1):
     return torch.maximum(torch.tensor(0), x @ w1 + b1)
 
-# Step 33 - apply_ffn_second_linear (not yet solved)
-# TODO: implement
+# Step 33 - apply_ffn_second_linear
+import torch
+
+def apply_ffn_second_linear(hidden, w2, b2):
+    return hidden @ w2 + b2
 
 # Step 34 - position_wise_feed_forward_network (not yet solved)
 # TODO: implement
