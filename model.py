7536e5fbf362053c7a931f8715646d375e98e3a6
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-07-01 21:41:34 +0530

    Step 40: encoder_layer_feed_forward_sublayer

--- model.py
+++ model.py
@@ -301,8 +301,10 @@ def encoder_layer_self_attention_sublayer(x, w_q, w_k, w_v, w_o, gamma, beta, nu
     x_mha = assemble_multi_head_attention_forward(x, x, x, w_q, w_k, w_v, w_o, num_heads, src_mask)
     return apply_residual_add_and_norm(x, x_mha, gamma, beta)
 
-# Step 40 - encoder_layer_feed_forward_sublayer (not yet solved)
-# TODO: implement
+# Step 40 - encoder_layer_feed_forward_sublayer
+def encoder_layer_feed_forward_sublayer(x, w1, b1, w2, b2, gamma, beta):
+    x_ffn = position_wise_feed_forward_network(x, w1, b1, w2, b2)
+    return apply_residual_add_and_norm(x, x_ffn, gamma, beta)
 
 # Step 41 - assemble_encoder_layer (not yet solved)
 # TODO: implement
