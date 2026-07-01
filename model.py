60ee133f7af041fd433e9dfea38b1aa3361e97da
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-07-01 21:57:53 +0530

    Step 42: stack_encoder_layers

--- model.py
+++ model.py
@@ -317,8 +317,11 @@ def assemble_encoder_layer(x, layer_params, num_heads, src_mask):
                                                      layer_params["ffn_gamma"], layer_params["ffn_beta"])
     return x_ff
 
-# Step 42 - stack_encoder_layers (not yet solved)
-# TODO: implement
+# Step 42 - stack_encoder_layers
+def stack_encoder_layers(x, encoder_layer_params_list, num_heads, src_mask):
+    for encoder_layer_params in encoder_layer_params_list:
+        x = assemble_encoder_layer(x, encoder_layer_params, num_heads, src_mask)
+    return x
 
 # Step 43 - decoder_layer_masked_self_attention_sublayer (not yet solved)
 # TODO: implement
