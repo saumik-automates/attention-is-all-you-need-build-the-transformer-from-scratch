6694d1370a5bb71187a5a1070150f8d2268f0679
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-07-01 22:01:58 +0530

    Step 43: decoder_layer_masked_self_attention_sublayer

--- model.py
+++ model.py
@@ -323,8 +323,12 @@ def stack_encoder_layers(x, encoder_layer_params_list, num_heads, src_mask):
         x = assemble_encoder_layer(x, encoder_layer_params, num_heads, src_mask)
     return x
 
-# Step 43 - decoder_layer_masked_self_attention_sublayer (not yet solved)
-# TODO: implement
+# Step 43 - decoder_layer_masked_self_attention_sublayer
+import torch
+
+def decoder_layer_masked_self_attention_sublayer(y, w_q, w_k, w_v, w_o, gamma, beta, num_heads, tgt_mask):
+    y_mha = assemble_multi_head_attention_forward(y, y, y, w_q, w_k, w_v, w_o, num_heads, tgt_mask)
+    return apply_residual_add_and_norm(y, y_mha, gamma, beta)
 
 # Step 44 - decoder_layer_cross_attention_sublayer (not yet solved)
 # TODO: implement
