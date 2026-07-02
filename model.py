1be49ee8800ed2ca874d6e0de205a0043d15bf10
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-07-02 18:13:57 +0530

    Step 48: apply_final_output_projection

--- model.py
+++ model.py
@@ -370,8 +370,9 @@ def stack_decoder_layers(y, encoder_output, decoder_layer_params_list, num_heads
         y = assemble_decoder_layer(y, encoder_output, decoder_layer_param, num_heads, src_mask, tgt_mask)
     return y
 
-# Step 48 - apply_final_output_projection (not yet solved)
-# TODO: implement
+# Step 48 - apply_final_output_projection
+def apply_final_output_projection(decoder_output, output_projection_weight, output_projection_bias=None):
+    return apply_linear_projection(decoder_output, output_projection_weight, output_projection_bias)
 
 # Step 49 - tie_output_projection_to_token_embeddings (not yet solved)
 # TODO: implement
