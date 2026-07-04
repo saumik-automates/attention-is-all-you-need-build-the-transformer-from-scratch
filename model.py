e2bc8b647c6b3a1dba24f8a2232e500d708e3044
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-07-04 23:39:25 +0530

    Step 58: build_uniform_smoothing_distribution

--- model.py
+++ model.py
@@ -551,8 +551,11 @@ def shift_targets_right_with_start_token(target_ids, start_token_id):
 def compute_noam_learning_rate(step, d_model, warmup_steps):
     return min(step**(-0.5), step*warmup_steps**(-3/2))*d_model**(-0.5)
 
-# Step 58 - build_uniform_smoothing_distribution (not yet solved)
-# TODO: implement
+# Step 58 - build_uniform_smoothing_distribution
+import torch
+
+def build_uniform_smoothing_distribution(shape, vocab_size, epsilon):
+    return torch.ones(shape)*epsilon/(vocab_size-2)
 
 # Step 59 - set_confidence_on_gold_tokens (not yet solved)
 # TODO: implement
